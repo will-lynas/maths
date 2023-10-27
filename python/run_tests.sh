d2955679cd1a0bbc31c13394d9640f7cdfe7100d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-10-28 00:31:03 +0100

    Improve run tests script

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
 echo "Running pytest"
-/usr/bin/env python -m pytest .
+/usr/bin/env python -m pytest . --no-header
+echo "Pytest passed"
+
+printf %"$COLUMNS"s |tr " " "-"
 echo "Running mypy"
 /usr/bin/env python -m mypy --strict maths_py
+echo "Mypy passed"
+
+printf %"$COLUMNS"s |tr " " "-"
 echo "Running pylint"
 /usr/bin/env python -m pylint maths_py
+echo "Pylint passed"
