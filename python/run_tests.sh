06b80d50d3c1e292439684ca06b4907808d69902
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-10-27 16:57:47 +0100

    Add docker

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 echo "Running pytest"
-venv/bin/python -m pytest .
+/usr/bin/env python -m pytest .
 echo "Running mypy"
-venv/bin/python -m mypy --strict maths_py
+/usr/bin/env python -m mypy --strict maths_py
 echo "Running pylint"
-venv/bin/python -m pylint maths_py
+/usr/bin/env python -m pylint maths_py
